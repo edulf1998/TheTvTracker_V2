1bf23e75646470d36deed2321f51d18726568953
Eduardo Liñares <edulf1998@gmail.com>
2020-04-30 14:44:59 +0200

    Primer commit - Proyecto básico creado.

---
+++ src/TheTvTracker_GUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace TheTvTracker_GUI.ViewModels
+{
+  public class ViewModelBase : ReactiveObject
+  {
+  }
+}
